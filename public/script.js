36e5a6af04f03fd664e5952331748ae839808795
Ander Dev <110144137+Anderson0421@users.noreply.github.com>
2023-11-08 12:29:58 -0500

    darkmodecheck

--- public/script.js
+++ public/script.js
@@ -31,29 +31,37 @@ if (isOpen) {
 });
 
 // Obteniendo los campos
+
 const btnSuns = document.querySelectorAll("#BtnSun");
-    let isDark = true;
-    for (const btnSun of btnSuns) {
+let isDark = true;
+
+for (const btnSun of btnSuns) {
     btnSun.addEventListener("click", () => {
         isDark = !isDark;
         const svgChange = btnSun.querySelector("svg");
+        const body = document.body;
 
         if (isDark) {
-        svgChange.innerHTML = `
+            svgChange.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
             </svg>
-            
-        `;
-
+            `;
         } else {
-        svgChange.innerHTML = `
+            svgChange.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
             </svg>
-        `;
+            `;
         }
-        document.body.classList.toggle('dark')
 
+        body.classList.toggle('dark');
+        body.classList.toggle('dark-mode-transition'); // Agregar/Quitar la clase de transición
+
+        // Espera a que termine la animación de transición y luego quita la clase de transición
+        setTimeout(() => {
+            body.classList.toggle('dark-mode-transition');
+        }, 300); // El mismo tiempo que la transición en CSS (0.3s)
     });
-    }
+}
+
